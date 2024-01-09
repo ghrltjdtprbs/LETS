6dab71bb258758bcd2f9dd7a20c769808d96ae78
ChangHo <tony9911@naver.com>
2024-01-09 15:53:28 +0900

    [feat] 주문 기능 구현

--- src/main/java/com/yanolja_final/domain/order/service/OrderService.java
+++ src/main/java/com/yanolja_final/domain/order/service/OrderService.java
@@ -1,21 +1,40 @@
 package com.yanolja_final.domain.order.service;
 
+import com.yanolja_final.domain.order.controller.request.OrderCreateRequest;
+import com.yanolja_final.domain.order.controller.response.OrderCreateResponse;
 import com.yanolja_final.domain.order.entity.Order;
-import com.yanolja_final.domain.order.exception.OrderNotFoundException;
 import com.yanolja_final.domain.order.repository.OrderRepository;
+import com.yanolja_final.domain.packages.entity.Package;
+import com.yanolja_final.domain.user.entity.User;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private static int dailyOrderCount = -1;
 
-    public Order findById(Long orderId) {
-        return orderRepository.findById(orderId)
-            .orElseThrow(() -> new OrderNotFoundException());
+    public OrderCreateResponse create(User user, Package aPackage,
+        OrderCreateRequest request) {
+        String code = generateDailyOrderCode();
+        Order order = request.toEntity(user, aPackage, code);
+        orderRepository.save(order);
+        return OrderCreateResponse.fromEntities(order, user);
+    }
+
+    private String generateDailyOrderCode() {
+        LocalDate today = LocalDate.now();
+        LocalDateTime lastOrderDateTime = orderRepository.findTopCreatedAt();
+        if (dailyOrderCount == -1 || !today.equals(lastOrderDateTime.toLocalDate())) {
+            dailyOrderCount = 0;
+        }
+
+        String orderCode = String.format("%04d%02d%02d%05d",
+            today.getYear(), today.getMonthValue(), today.getDayOfMonth(), ++dailyOrderCount);
+        return orderCode;
     }
 }
