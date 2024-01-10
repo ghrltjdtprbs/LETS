12b6b0e776f30b9a4a2f99aa8f6e4c5f774804ed
JeongByungJoo <cheng286@naver.com>
2024-01-10 15:34:36 +0900

    [feat] Notice Entity 수정으로 categories column 추가 ->
    Notice Entity 클래스의 생성자에 categories 추가(Entity 수정됨)
    
    NoticeResponse, RegisterNoticeRequest에 categories 추가 및 String[] content와 String[] categories에 여백 제거를 위한 trim 적용

--- src/main/java/com/yanolja_final/domain/notice/dto/request/RegisterNoticeRequest.java
+++ src/main/java/com/yanolja_final/domain/notice/dto/request/RegisterNoticeRequest.java
@@ -2,21 +2,32 @@ package com.yanolja_final.domain.notice.dto.request;
 
 import com.yanolja_final.domain.notice.entity.Notice;
 import jakarta.validation.constraints.NotNull;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import javax.print.DocFlavor.STRING;
+import org.aspectj.weaver.ast.Not;
 
 public record RegisterNoticeRequest(
     @NotNull
     String title,
     @NotNull
-    String[] content
+    String[] content,
+    @NotNull
+    String[] categories
 ) {
 
     public Notice toEntity() {
-
-        String splitContent = String.join("\n", content);
+        String splitContent = Arrays.stream(content)
+            .map(String::trim)
+            .collect(Collectors.joining("\n"));
+        String splitCategories = Arrays.stream(categories)
+            .map(String::trim)
+            .collect(Collectors.joining(","));
 
         return Notice.builder()
             .title(title)
             .content(splitContent)
+            .categories(splitCategories)
             .build();
     }
 }
--- src/main/java/com/yanolja_final/domain/notice/dto/response/NoticeResponse.java
+++ src/main/java/com/yanolja_final/domain/notice/dto/response/NoticeResponse.java
@@ -7,20 +7,21 @@ public record NoticeResponse(
     Long noticeId,
     String title,
     String createdAt,
-    String[] content
-
+    String[] content,
+    String[] categories
 ) {
-
 
     public static NoticeResponse fromNotice(Notice notice) {
 
         String[] splitContent = notice.getContent().split("\n");
+        String[] splitCategories = notice.getCategories().split(",");
 
         return new NoticeResponse(
             notice.getId(),
             notice.getTitle(),
             notice.getFormattedDate(),
-            splitContent
+            splitContent,
+            splitCategories
         );
     }
 }
