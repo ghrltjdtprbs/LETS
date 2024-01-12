127254bb95bf925a42ee8bda490cc76bae5bfef1
deepredk <deepred.kim@gmail.com>
2024-01-12 10:59:00 +0900

    [feat] DTO Validation 실패 시 어떤 정보가 잘못됐는지 메시지 자세히 출력

--- src/main/java/com/yanolja_final/global/exception/GlobalExceptionRestAdvice.java
+++ src/main/java/com/yanolja_final/global/exception/GlobalExceptionRestAdvice.java
@@ -1,11 +1,20 @@
 package com.yanolja_final.global.exception;
 
 import com.yanolja_final.global.util.ResponseDTO;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.NestedExceptionUtils;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,12 +31,34 @@ public class GlobalExceptionRestAdvice {
     }
 
     @ExceptionHandler
-    public ResponseEntity<ResponseDTO<Void>> bindException(BindException e) {
-        log.error(e.getMessage(), e);
-        return ResponseEntity
-            .status(HttpStatus.BAD_REQUEST)
-            .body(ResponseDTO.errorWithMessage(HttpStatus.BAD_REQUEST,
-                e.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
+    public ResponseDTO<Void> validationException(BindException e) {
+        log.warn("[ValidationException] Message = {}",
+            NestedExceptionUtils.getMostSpecificCause(e).getMessage());
+
+        BindingResult bindingResult = e.getBindingResult();
+        List<FieldError> fieldErrors = getSortedFieldErrors(bindingResult);
+
+        String response = "[Request error] "
+            + fieldErrors.stream()
+            .map(fieldError -> String.format("%s (%s=%s)",
+                    fieldError.getDefaultMessage(),
+                    fieldError.getField(),
+                    fieldError.getRejectedValue()
+                )
+            ).collect(Collectors.joining());
+        return ResponseDTO.errorWithMessage(HttpStatus.BAD_REQUEST, response);
+    }
+
+    private List<FieldError> getSortedFieldErrors(BindingResult bindingResult) {
+        List<String> declaredFields = Arrays.stream(
+                Objects.requireNonNull(bindingResult.getTarget()).getClass().getDeclaredFields())
+            .map(Field::getName)
+            .toList();
+
+        return bindingResult.getFieldErrors().stream()
+            .filter(fieldError -> declaredFields.contains(fieldError.getField()))
+            .sorted(Comparator.comparingInt(fe -> declaredFields.indexOf(fe.getField())))
+            .collect(Collectors.toList());
     }
 
     @ExceptionHandler
