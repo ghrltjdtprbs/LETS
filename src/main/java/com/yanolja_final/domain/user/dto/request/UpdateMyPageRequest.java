55922871bde312f339422e8a16cfd2f2381b75e8
JeongByungJoo <cheng286@naver.com>
2024-01-13 10:20:13 +0900

    [feat] #UpdateMyPageReqeust
    # UpdateMyPageReqeust에서dateSelectiveInfo 라는 리퀘스트의 요청 필드를 일일이 set하여 업데이트 하던 메서드를 제거 ---> User Entity에 updateUserInfo 라는 메서드 추가하여 기존에 set으로 일일이 불러오던 메서드의 로직을 제거함(@Setter도 제거)
    
    #UpdateMyPageRequest 클래스의 일반 숫자 00011114444의 형태를 000-1111-4444로 바꿔주던 formatPhoneNumber라는 메서드를 제거하고, phone field에 (010-0000-0000)를 검증하는 Annotation 추가

--- src/main/java/com/yanolja_final/domain/user/dto/request/UpdateMyPageRequest.java
+++ src/main/java/com/yanolja_final/domain/user/dto/request/UpdateMyPageRequest.java
@@ -1,29 +1,13 @@
 package com.yanolja_final.domain.user.dto.request;
 
-import com.yanolja_final.domain.user.entity.User;
+import jakarta.validation.constraints.Pattern;
 
 public record UpdateMyPageRequest(
+    @Pattern(regexp = "\\d{3}-\\d{4}-\\d{4}", message = " 010-0000-0000 형식으로 입력해주세요 ")
     String phone,
     String addr1,
     String addr2,
     String postCode
 ) {
-    public void updateSelectiveInfo(User existingUser) {
-        if (phone != null) {
-            existingUser.setPhoneNumber(formatPhoneNumber(phone));
-        }
-        if (addr1 != null) {
-            existingUser.setAddr1(addr1);
-        }
-        if (addr2 != null) {
-            existingUser.setAddr2(addr2);
-        }
-        if (postCode != null) {
-            existingUser.setPostCode(postCode);
-        }
-    }
 
-    private String formatPhoneNumber(String rawPhoneNumber) {
-        return rawPhoneNumber.replaceAll("(\\d{3})(\\d{4})(\\d{4})", "$1-$2-$3");
-    }
 }
