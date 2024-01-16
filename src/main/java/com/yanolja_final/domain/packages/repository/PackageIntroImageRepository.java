f07e02efd059997772dc73de0877c55dd96276a3
hym <ghrltjdtprbs@gmail.com>
2024-01-16 17:40:58 +0900

    [refactor] 불필요한 클래스 삭제
    문제 사항 :
     - 리팩토링을 진행해 PackageIntroImageRepository클래스가 필요 없어짐
    변경 사항 :
     - PackageIntroImageRepository 삭제

--- src/main/java/com/yanolja_final/domain/packages/repository/PackageIntroImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.yanolja_final.domain.packages.repository;
-
-import com.yanolja_final.domain.packages.entity.PackageIntroImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PackageIntroImageRepository extends JpaRepository<PackageIntroImage, Long> {
-
-}
