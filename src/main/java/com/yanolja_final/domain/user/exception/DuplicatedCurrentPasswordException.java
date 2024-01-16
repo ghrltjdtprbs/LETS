b0b1338d4a1473b3475cfe2a369390997a61bf12
JeongByungJoo <cheng286@naver.com>
2024-01-16 12:27:36 +0900

    [feat] 비밀번호 변경 시 현재와 동일한 비밀번호를 입력한 경우 예외 발생 _ DUPLICATED_CURRENT_PASSWORD

---
+++ src/main/java/com/yanolja_final/domain/user/exception/DuplicatedCurrentPasswordException.java
@@ -0,0 +1,12 @@
+package com.yanolja_final.domain.user.exception;
+
+import com.yanolja_final.global.exception.ApplicationException;
+import com.yanolja_final.global.exception.ErrorCode;
+
+public class DuplicatedCurrentPasswordException extends ApplicationException {
+
+    private static final ErrorCode ERROR_CODE = ErrorCode.DUPLICATED_CURRENT_PASSWORD;
+
+    public DuplicatedCurrentPasswordException() { super(ERROR_CODE); }
+
+}
