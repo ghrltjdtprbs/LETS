420f92332a77a46931d5aec6007ed55286a5ca40
JeongByungJoo <cheng286@naver.com>
2024-01-23 01:27:46 +0900

    [feat] 다가오는 패키지를 조회하는 getUpcomingPackage 메서드 추가

--- src/main/java/com/yanolja_final/domain/user/facade/MyPageFacade.java
+++ src/main/java/com/yanolja_final/domain/user/facade/MyPageFacade.java
@@ -1,8 +1,12 @@
 package com.yanolja_final.domain.user.facade;
 
+import com.yanolja_final.domain.order.entity.Order;
+import com.yanolja_final.domain.packages.entity.PackageDepartureOption;
 import com.yanolja_final.domain.user.dto.request.UpdateMyPageRequest;
 import com.yanolja_final.domain.user.dto.request.UpdatePasswordRequest;
 import com.yanolja_final.domain.user.dto.response.MyPageResponse;
+import com.yanolja_final.domain.user.dto.response.UpcomingPackageResponse;
+import com.yanolja_final.domain.user.entity.User;
 import com.yanolja_final.domain.user.service.MyPageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,5 +29,24 @@ public class MyPageFacade {
 
     public void updatePassword(UpdatePasswordRequest request, Long userId) {
         myPageService.updatePassword(request, userId);
+    }
+
+    public UpcomingPackageResponse getUpcomingPackageResponse(Long userId) {
+        User user = userId  == null ? null : myPageService.findById(userId);
+        Order userOrder = Order.userOrderWithEarliestDepartureDate(user);
+        PackageDepartureOption packageDepartureOption = userOrder.getPackageDepartureOption();
+        Long dday = packageDepartureOption.calculateDday();
+        String departureDate = packageDepartureOption.formattedDepartureDate();
+        String endDate = packageDepartureOption.formattedEndDate();
+
+        return new UpcomingPackageResponse(
+            userOrder.getAPackage().getId(),
+            userOrder.getAPackage().getThumbnailImageUrl(),
+            userOrder.getAPackage().getTitle(),
+            dday,
+            userOrder.getAPackage().getNationName(),
+            departureDate,
+            endDate
+        );
     }
 }
