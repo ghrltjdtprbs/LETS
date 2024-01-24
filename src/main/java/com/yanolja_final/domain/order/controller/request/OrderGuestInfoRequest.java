46bf76fde3feabc8be95d376b96d5f2e6e0ade16
ChangHo <tony9911@naver.com>
2024-01-25 05:00:23 +0900

    [feat] 예약시 사람 수 / 취소 수수료 false 관련 기능 처리

--- src/main/java/com/yanolja_final/domain/order/controller/request/OrderGuestInfoRequest.java
+++ src/main/java/com/yanolja_final/domain/order/controller/request/OrderGuestInfoRequest.java
@@ -1,8 +1,13 @@
 package com.yanolja_final.domain.order.controller.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record OrderGuestInfoRequest(
+    @NotNull
     Integer adult,
+    @NotNull
     Integer infant,
+    @NotNull
     Integer baby
 ) {
 
