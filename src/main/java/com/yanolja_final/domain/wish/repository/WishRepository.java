9811ab76bb2d7eab21861ea63647b752ec0af15f
ChangHo <tony9911@naver.com>
2024-01-26 03:38:04 +0900

    [fix] 예약 내역 isWish/isReviewed 동작 안하는 오류 수정

--- src/main/java/com/yanolja_final/domain/wish/repository/WishRepository.java
+++ src/main/java/com/yanolja_final/domain/wish/repository/WishRepository.java
@@ -6,20 +6,14 @@ import com.yanolja_final.domain.wish.entity.Wish;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WishRepository extends JpaRepository<Wish, Long> {
 
     Optional<Wish> findByIdAndUserId(Long wishId, Long userId);
 
     boolean existsByUserAndAPackage(User user, Package aPackage);
-
-    @Query("SELECT CASE WHEN COUNT(w) > 0 THEN true ELSE false END FROM Wish w " +
-        "WHERE w.user.id = :userId AND w.APackage.id = :packageId")
-    boolean isUserWishingPackage(@Param("userId") Long userId, @Param("packageId") Long packageId);
 
     Optional<Wish> findByAPackage_IdAndUser_Id(Long packageId, Long userId);
 
